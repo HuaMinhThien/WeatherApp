9c81c5ce19cc6e756ffbf2e97084b07995e97ec5
HuaMinhThien <huaminhthien.18022019@gmail.com>
2026-03-15 09:27:32 +0700

    d4 1.1

--- src/function/sec2-daily-forecast.ts
+++ src/function/sec2-daily-forecast.ts
@@ -8,12 +8,25 @@ export const dailyWeather = (data_DailyWeather: DailyWeather) => {
     
     const weekdays = data_DailyWeather.time.map((time) => {
         const date = new Date(time);
-        return date.toLocaleDateString("en-US", { weekday: "long" });
+        return date.toLocaleDateString("en-US", { weekday: "short" });
     });
 
-    sectionDailyWeather.innerHTML = ``;
+    let html = ``;
+
+    weekdays.forEach((weekday, index) => {
+        html += `
+        <div class="sec2-daily-box">
+            <p>${weekday}</p>
+            <img src="${weatherStatus(data_DailyWeather.weather_code[index] ?? 0)}" alt="" />
+            <div class="sec2-daily-box-temperature">
+              <p>${Math.round(data_DailyWeather.temperature_2m_max[index] ?? 0)}°</p>
+              <p>${Math.round(data_DailyWeather.temperature_2m_min[index] ?? 0)}°</p>
+            </div>
+          </div>
+          `;
+    });
 
     
-    
-
+    return sectionDailyWeather.innerHTML = html;
 }
+
--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,17 @@ const inputSearch = document.getElementById("search") as HTMLInputElement;
 const btnSearch = document.getElementById("btn-search") as HTMLButtonElement;
 
 btnSearch?.addEventListener("click",async function(){
+
     const value:string = inputSearch.value.trim().toLocaleLowerCase();
     const location = value.replaceAll(" ", "+");
-    console.log(location);
-
-    console.log(getToaDo(location));
     
     // API data
     const res_toaDo:Location = await getToaDo(location);
+    if (!res_toaDo) {
+        alert("Location not found. Please try again.");
+        return;
+    }
     const res_getWeather:WeatherResponse = await getWeather(res_toaDo.lat, res_toaDo.lon);
-    console.log(res_getWeather);
     
 
     currentWeather(res_toaDo.name || 'Unknown Location', res_getWeather.current);
